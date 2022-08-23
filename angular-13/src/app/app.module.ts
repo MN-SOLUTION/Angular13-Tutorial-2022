1a88c6f530941fe23318af64e1dc1c8716b5cade
Salman Masood <salman.masood@10pearls.com>
2022-08-23 17:06:51 +0500

    develop- Partial commit

--- angular-13/src/app/app.module.ts
+++ angular-13/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {EmployeeComponent} from './employee/employee.component'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+    
+  ]
 })
 export class AppModule { }
